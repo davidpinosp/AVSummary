683bb2936391683107a4a2ee3b48531517ebb175
davidpinosp <davidspinosp@gmail.com>
2023-05-11 19:50:48 -0400

    Feature: Add HomeScreen,Navbar,LoginBox
    Addiing the landing page as well as some routes.

--- website/views.py
+++ website/views.py
@@ -1,20 +1,23 @@
 import json
-from flask import Flask, render_template, request,redirect,url_for,Blueprint,jsonify
+from flask import Flask, render_template, request, redirect, url_for, Blueprint, jsonify
 
 # //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
 
 
-# ToDO : finish front end (loading and formatting), hide keys , launch , add ads. 
+# ToDO : finish front end (loading and formatting), hide keys , launch , add ads.
 
 
 # input a video and get it to translate the text then summarize the text
 
 
-
-views = Blueprint('views', __name__) 
-
+views = Blueprint('views', __name__)
 
 
 @views.route('/')
 def index():
     return render_template('index.html')
+
+
+@views.route('/login')
+def login():
+    return render_template('login.html')
