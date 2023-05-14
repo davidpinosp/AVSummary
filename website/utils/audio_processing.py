66ec5e7f028e574b271f4224cdd77103f0dcbe4d
davidpinosp <davidspinosp@gmail.com>
2023-05-14 19:29:42 -0400

    Feature: Save/Delete Summary
    Adding the ability to save/delete from db . called notes for convenience

--- website/utils/audio_processing.py
+++ website/utils/audio_processing.py
@@ -1,6 +1,7 @@
 import os
 from google.cloud import speech_v1p1beta1 as speech
-
+import keys
+import openai
 # Set up Google Cloud credentials
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv(
@@ -16,10 +17,10 @@ def transcribe_audio(audio_file):
 
     audio = speech.RecognitionAudio(content=content)
     config = speech.RecognitionConfig(
-        sample_rate_hertz=44100,
+        # sample_rate_hertz=44100,
         enable_automatic_punctuation=True,
         language_code='en-US',
-        audio_channel_count=2
+        # audio_channel_count=2
     )
 
     # Perform the speech-to-text transcription
@@ -33,11 +34,20 @@ def transcribe_audio(audio_file):
 
 
 def summarize_text(text):
-    # You can use your preferred text summarization algorithm here
-    # This is a simple example that extracts the first few sentences as a summary
-    sentences = text.split('. ')
-    # Adjust the number of sentences for your desired summary length
-    summary = '. '.join(sentences[:3])
+
+    openai.api_key = keys.API_KEY
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"Summarize the following text: {text}",
+        max_tokens=100,
+        temperature=0.3,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )
+
+    summary = response.choices[0].text.strip()
     return summary
 
 
@@ -46,11 +56,11 @@ def get_transcription_and_summary(filepath):
     audio_file = filepath
     print(f"Processing audio file: {audio_file}")
     transcript = transcribe_audio(audio_file)
-    # summary = summarize_text(transcript)
+    summary = summarize_text(transcript)
 
-    # print("Transcript:")
-    # print(transcript)
-    # print("Summary:")
-    # print(summary)
-    # print('\n')
-    return transcript
+    print("Transcript:")
+    print(transcript)
+    print("Summary:")
+    print(summary)
+    print('\n')
+    return summary
--- website/views.py
+++ website/views.py
@@ -3,14 +3,9 @@ from flask import Flask, render_template, request, redirect, url_for, Blueprint,
 from .utils.audio_processing import get_transcription_and_summary
 import tempfile
 import os
+from .models import Note
+from . import db
 from flask_login import login_user, login_required, logout_user, current_user
-# //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
-
-
-# ToDO : finish front end (loading and formatting), hide keys , launch , add ads.
-
-
-# input a video and get it to translate the text then summarize the text
 
 
 views = Blueprint('views', __name__)
@@ -22,10 +17,38 @@ def index():
     return render_template('index.html', user=current_user)
 
 
-@views.route('main')
+@views.route('/main', methods=['GET', 'POST'])
 @login_required
 def main():
     return render_template('main.html', user=current_user)
+
+
+@views.route('/save-input', methods=['POST'])
+@login_required
+def save_input():
+    note = request.form.get('resultText')
+    # Perform the desired action with the input value
+    print("Input value:", note)
+    # You can store the value in a database, send it to another route, or perform any other operation here
+    if len(note) < 1:
+        flash('ID is too short!', category='error')
+    else:
+        # providing the schema for the note
+        new_note = Note(data=note, user_id=current_user.id)
+        db.session.add(new_note)  # adding the note to the database
+        db.session.commit()
+
+        # insert logic here navigate to next page that pulls image into the html
+        flash('Your summary has been saved!', category='success')
+        # render another thing
+
+    return redirect(url_for('views.main'))
+
+
+@views.route('notes')
+@login_required
+def notes():
+    return render_template('notes.html', user=current_user)
 
 
 @views.route('/process-audio', methods=['POST'])
@@ -45,3 +68,18 @@ def process_audio():
 
     # Return the result
     return {'transcript': transcript}
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    # get_points(1718560715)
+    # this function expects a JSON from the INDEX.js file
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
